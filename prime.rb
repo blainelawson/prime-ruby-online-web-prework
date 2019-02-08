079d58af81db68ef5e6d0e9a8079d1213eae0bb6
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 22:47:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime?(number)
+  return false if !number.integer?
+  
+  return false if number < 2
+  
+  div_count = 0
+  range1 = (1..number)
+  array1 = range1.to_a
+  array1.each do |i|
+    # I want to now divide this i by all numbers in the range UP TO i
+    range1 = (1..i)
+    array2 = range1.to_a
+    array2.each do |inner_i|
+      if inner_i % i == 0
+        div_count += 1
+      end
+    end
+  end
+  if div_count > 2
+    false
+  else
+    true
+  end
+end
+  
+
