c409d78398d8691d167880711622374d86752780
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 22:51:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,7 @@ def prime?(number)
   return false if number < 2
   
   div_count = 0
-  range1 = (1..number)
-  array1 = range1.to_a
-  array1.each do |i|
+  (1..number).each do |i|
     # I want to now divide this i by all numbers in the range UP TO i
     if number % i == 0
       div_count += 1
