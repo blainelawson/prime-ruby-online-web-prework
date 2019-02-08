d81eef50dbf25c7294cbc15716a21c95edae908a
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-08 23:01:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,16 +3,10 @@ def prime?(number)
   
   return false if number < 2
   
-  div_count = 0
-  (1..number-1).each do |i|
+  (2..number-1).each do |i|
     # I want to now divide this i by all numbers in the range UP TO i
-    if number % i == 0
-      div_count += 1
-    end
+    return false if number % i == 0
   end
-  if div_count > 2
-    false
-  else
     true
   end
 end
